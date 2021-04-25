f79eba1c7f362a08e3e5cd6291d57248c6a6afa0
NEUQ <21213372+w4c4dwsz@users.noreply.github.com>
2021-04-25 12:59:20 +0800

    2021-04-25 test

--- lio_sam_data_node.cpp
+++ lio_sam_data_node.cpp
@@ -2,11 +2,24 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <iostream>
 
+/**
+ * @brief add ring and times fileds to the poincloud2 msgs
+ * 
+ * @param inputLidarData 
+ */
+void lidarCallback(const sensor_msgs::PointCloud2 inputLidarData) {
+    inputLidarData
+}
+
 int main(int argc, char** argv) 
 {
     ros::init(argc, argv, "lio_sam_data_node");
 
     ros::NodeHandle nh;
 
-    ros::Publisher  lioSamInputLidarData = nh.advertise
+    
+
+    ros::Publisher  lioSamInputLidarData = nh.advertise<sensor_msgs::PointCloud2>("lidar_output", 100);
+
+    ros::Subscriber lioSamOutPutLidatData = nh.subscribe<sensor_msgs::PointCloud2>("sf", 100, lidarCallback);
 }
